6502f361302561275aa113373f76c28449f1677c
Natalie Akam <natalie.akam@gmail.com>
2015-07-25 12:49:42 +0100

    Added basic average_rating method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,8 @@ class Restaurant < ActiveRecord::Base
   belongs_to :user
   validates :name, length: {minimum: 3}, uniqueness: true
 
+  def average_rating
+    return 'N/A' if reviews.none?
+    4
+  end
 end
